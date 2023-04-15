41e4323b5601c82be2e5f64ed52af034ad348043
biyakim <w2116@e-mirim.hs.kr>
2023-04-15 13:55:28 +0900

    수정 다함

--- src/page/Communication.js
+++ src/page/Communication.js
@@ -3,15 +3,12 @@ import {Route} from 'react-router-dom';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import logo from '../img/logo.png'
-import profile from '../img/profile.png';
+import mom from '../img/mom.png';
 import React, { useState } from 'react';
-<<<<<<< HEAD:src/Communication.js
 import styled from 'styled-components';
-import heart from './heart.png';
-import Vector from './Vector.png';
-=======
+import heart from '../img/heart.png';
+import Vector from '../img/Vector.png';
 
->>>>>>> 2c0bdf1b5c4bdaa3d4f51b6de00dedec2c478145:src/page/Communication.js
 
 const Communication = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -32,11 +29,9 @@ const Communication = () => {
                     <li><a href=''>소통방</a></li>
                     <li><a href=''>육아팁</a></li>
                     <li><a href=''>다이어리</a></li>
-                    <a href='#' className='navbar_toggleBtn' onClick={toggleMenu}>
-                        <li><img src={profile}/></li>
-                    </a>
+                    <li><img src={mom}/></li>
                 </ul>
-                <a href='#' className='navbar_toggleBtn'>
+                <a href='#' className='navbar_toggleBtn' onClick={toggleMenu}>
                     <FontAwesomeIcon icon={faBars} />
                 </a>
             </nav>
