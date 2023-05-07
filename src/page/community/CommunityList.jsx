a675659ae4aa3facdbfcbcd18d7227d2c6479b37
yejin <yejin213527@gmail.com>
2023-05-07 21:54:58 +0900

    소통

--- src/page/community/CommunityList.jsx
+++ src/page/community/CommunityList.jsx
@@ -1,9 +1,6 @@
-import './Communication.css';
+import './communityList.css';
 import { Link } from 'react-router-dom';
 import React, { useState } from 'react';
-import heart from '../../img/heart.png';
-import communi from '../../img/communi.png';
-import pink_heart from '../../img/pink_heart.png';
 import styled from "styled-components";
 
 
@@ -11,46 +8,28 @@ const CommunityList = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const [isClick, setIsClick] = useState(false);
 
-   
+
 
     const click_heart = () => {
         setIsClick(!isClick);
     }
 
-   
+
     return (
         <div>
-
-            <body>
-            <>
-            <Button href="/community/posting">당신의 이야기를 들려주세요</Button>
-            </>
-
-                <div className='communi-box'>
-                    산후 우울증에 대해...
-                               
-
-                <img src={isClick? pink_heart: heart} alt='img' onClick={click_heart} className='heart'/>
-                    
-                    <img className='communi' src={communi} alt="communi"/>
-                    
-                </div>
-            </body>
+            <p className='p'>제목</p>
+            <hr className='hr' size="3" color='#FDEBC7'></hr>
+            <p className='p'>제목</p>
+            <hr className='hr' size="3" color='#FDEBC7'></hr>
+            <p className='p'>제목</p>
+            <hr className='hr' size="3" color='#FDEBC7'></hr>
+            <p className='p'>제목</p>
+            <hr className='hr' size="3" color='#FDEBC7'></hr>
+            <p className='p'>제목</p>
+            <hr className='hr' size="3" color='#FDEBC7'></hr>
+            <p className='p'>제목</p>
+            <hr className='hr' size="3" color='#FDEBC7'></hr>
         </div>
     );
 };
-const Button = styled.a`
-    width: 100px;
-    margin: auto;
-    display: block;
-    margin-top: 100px;
-    width: 500px;
-    height: 30px;
-    border-radius: 30px;
-    background-color: #FFF9E3;
-    text-align: center;
-    text-decoration: none;
-    box-shadow: 5px 5px 5px rgb(145, 145, 145);
-    color: #F6D697;
-`;
 export default CommunityList;
