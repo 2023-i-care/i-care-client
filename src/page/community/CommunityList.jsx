dd1d2d73e24363261a1b0b41487fd1e3f6f6e6cf
yejin <yejin213527@gmail.com>
2023-05-08 09:12:56 +0900

    소통수정

--- src/page/community/CommunityList.jsx
+++ src/page/community/CommunityList.jsx
@@ -1,35 +1,51 @@
-import './communityList.css';
-import { Link } from 'react-router-dom';
-import React, { useState } from 'react';
+import "./communityList.css";
+import { Link } from "react-router-dom";
+import React, { useState } from "react";
 import styled from "styled-components";
-
+import img from "../../img/baby.jpg";
 
 const CommunityList = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const [isClick, setIsClick] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isClick, setIsClick] = useState(false);
 
+  const click_heart = () => {
+    setIsClick(!isClick);
+  };
 
-
-    const click_heart = () => {
-        setIsClick(!isClick);
-    }
-
-
-    return (
-        <div>
-            <p className='p'>제목</p>
-            <hr className='hr' size="3" color='#FDEBC7'></hr>
-            <p className='p'>제목</p>
-            <hr className='hr' size="3" color='#FDEBC7'></hr>
-            <p className='p'>제목</p>
-            <hr className='hr' size="3" color='#FDEBC7'></hr>
-            <p className='p'>제목</p>
-            <hr className='hr' size="3" color='#FDEBC7'></hr>
-            <p className='p'>제목</p>
-            <hr className='hr' size="3" color='#FDEBC7'></hr>
-            <p className='p'>제목</p>
-            <hr className='hr' size="3" color='#FDEBC7'></hr>
-        </div>
-    );
+  return (
+    <div>
+      <p className="p">
+        <img className="img" src={img} />
+        제목
+      </p>
+      <p className="p2">작성자 아이디 | 2023.05.08</p>
+      <hr className="hr" size="3" color="#FDEBC7"></hr>
+      <p className="p">
+        <img className="img" src={img} />
+        제목
+      </p>
+      <hr className="hr" size="3" color="#FDEBC7"></hr>
+      <p className="p">
+        <img className="img" src={img} />
+        제목
+      </p>
+      <hr className="hr" size="3" color="#FDEBC7"></hr>
+      <p className="p">
+        <img className="img" src={img} />
+        제목
+      </p>
+      <hr className="hr" size="3" color="#FDEBC7"></hr>
+      <p className="p">
+        <img className="img" src={img} />
+        제목
+      </p>
+      <hr className="hr" size="3" color="#FDEBC7"></hr>
+      <p className="p">
+        <img className="img" src={img} />
+        제목
+      </p>
+      <hr className="hr" size="3" color="#FDEBC7"></hr>
+    </div>
+  );
 };
 export default CommunityList;
