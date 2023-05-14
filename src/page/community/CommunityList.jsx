2602c3d367e7799697c8cfd3573d385d10c2c1dc
yejin <yejin213527@gmail.com>
2023-05-14 23:22:40 +0900

    소통 리스트 수정

--- src/page/community/CommunityList.jsx
+++ src/page/community/CommunityList.jsx
@@ -13,48 +13,50 @@ const CommunityList = () => {
   };
 
   return (
-    <table>
-      <thead>
-        <tr className="tr">
+    <div>
+      <table>
+        <thead>
+          <tr className="tr">
 
-          <th>제목</th>
-          <th>등록일</th>
-          <th>조회수</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr className="tr">
-          <img className="img" src={img} />
-          <td>첫번째 게시글입니다.</td>
-          <td>2020-10-25</td>
-          <td>6</td>
-        </tr>
-        <tr className="tr">
-          <img className="img" src={img} />
-          <td>두번째 게시글입니다.</td>
-          <td>2020-10-25</td>
-          <td>5</td>
-        </tr>
-        <tr className="tr">
-          <img className="img" src={img} />
-          <td>세번째 게시글입니다.</td>
-          <td>2020-10-25</td>
-          <td>1</td>
-        </tr>
-        <tr className="tr">
-          <img className="img" src={img} />
-          <td>네번째 게시글입니다.</td>
-          <td>2020-10-25</td>
-          <td>2</td>
-        </tr>
-        <tr className="tr">
-          <img className="img" src={img} />
-          <td>다섯번째 게시글입니다.</td>
-          <td>2020-10-25</td>
-          <td>4</td>
-        </tr>
-      </tbody>
-    </table>
+            <th>제목</th>
+            <th>등록일</th>
+            <th>조회수</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="tr">
+            <img className="img" src={img} />
+            <td>첫번째 게시글입니다.</td>
+            <td>2020-10-25</td>
+            <td>6</td>
+          </tr>
+          <tr className="tr">
+            <img className="img" src={img} />
+            <td>두번째 게시글입니다.</td>
+            <td>2020-10-25</td>
+            <td>5</td>
+          </tr>
+          <tr className="tr">
+            <img className="img" src={img} />
+            <td>세번째 게시글입니다.</td>
+            <td>2020-10-25</td>
+            <td>1</td>
+          </tr>
+          <tr className="tr">
+            <img className="img" src={img} />
+            <td>네번째 게시글입니다.</td>
+            <td>2020-10-25</td>
+            <td>2</td>
+          </tr>
+          <tr className="tr">
+            <img className="img" src={img} />
+            <td>다섯번째 게시글입니다.</td>
+            <td>2020-10-25</td>
+            <td>4</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 };
 export default CommunityList;
