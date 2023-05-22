47b84b65f066f63218d03ff267ee480cb2672ae9
Hyemin Choi <90095211+hyennin@users.noreply.github.com>
2023-05-22 12:15:28 +0900

    파일 변경

--- src/pages/community/post.jsx
+++ src/pages/community/post.jsx
@@ -6,14 +6,6 @@ const CommunityPost = () => {
 	return (
 		<>
 			<Navbar/>
-			<div className={styles.container}>
-				<input className={styles.title} type='text' placeholder="제목을 입력하세요"/>
-				<textarea className={styles.content} type='text' placeholder="내용을 입력하세요"/>
-				<div className={styles.btn_container}>
-					<button className={styles.img_btn}>사진 업로드</button>
-					<button className={styles.post_btn}>게시하기</button>
-				</div>
-			</div>
 		</>
 	);
 }
