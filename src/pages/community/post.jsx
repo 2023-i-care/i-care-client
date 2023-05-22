21fb337e1dba29939b956ee824be89db49625a0a
Hyemin Choi <90095211+hyennin@users.noreply.github.com>
2023-05-22 12:14:28 +0900

    community post ui 구현

--- src/pages/community/post.jsx
+++ src/pages/community/post.jsx
@@ -1,12 +1,21 @@
+import Navbar from "@/components/Navbar";
 import React from "react";
 import styles from "../../styles/CommunityPost.module.css";
 
 const CommunityPost = () => {
-    return (
-        <>
-            <h1>소통 글</h1>
-        </>
-    );
+	return (
+		<>
+			<Navbar/>
+			<div className={styles.container}>
+				<input className={styles.title} type='text' placeholder="제목을 입력하세요"/>
+				<textarea className={styles.content} type='text' placeholder="내용을 입력하세요"/>
+				<div className={styles.btn_container}>
+					<button className={styles.img_btn}>사진 업로드</button>
+					<button className={styles.post_btn}>게시하기</button>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default CommunityPost;
