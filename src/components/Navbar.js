a6e915ba8afcebd0cc7f6a02bb9f72b3a485531c
Hyemin Choi <90095211+hyennin@users.noreply.github.com>
2023-05-22 10:24:25 +0900

    navbar ui 구현

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from '@/styles/Navbar.module.css';
+
+
+const Navbar = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.left_container}>
+                <a href='/'><img src='/images/logo.png'/></a>
+                <div className={styles.search_container}>
+                    <input className={styles.search_box}/>
+                    <img className={styles.search_icon} src='/images/search.png'/>
+                </div>
+            </div>
+            <div className={styles.right_container}>
+                <div className={styles.text}><a href='/community'>소통방</a></div>
+                <div className={styles.text}><a href='/diary'>다이어리</a></div>
+                <div><a className={styles.profile} href='/mypage'><img src='/images/profile.png'/></a></div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
+import Navbar from '@/components/Navbar';
 import React from 'react';
 import Login from './login';
 
 export default function Home() {
   return (
-    <Login/>
+    <Navbar/>
   );
 }
