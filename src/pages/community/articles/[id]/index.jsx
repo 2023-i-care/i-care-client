032be080930d575d5ea6111cae62d687e3e2bbfc
biyakim <w2116@e-mirim.hs.kr>
2023-06-06 00:46:42 +0900

    댓글 다 다르게 하기 성공

--- src/pages/community/articles/[id]/index.jsx
+++ src/pages/community/articles/[id]/index.jsx
@@ -1,58 +1,67 @@
 import Navbar from "@/components/Navbar";
 import db from "@/net/db";
-import { doc, getDoc, onSnapshot, query,collection,orderBy, addDoc } from "firebase/firestore";
+import { doc, getDoc, onSnapshot, query, collection, orderBy, addDoc, where } from "firebase/firestore";
 import { useRouter } from "next/router";
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import styles from "../../../../styles/CommunityPost.module.css";
 
 export default function Article() {
-    const router = useRouter();
-    const [subject, setSubject] = useState('');
-    const [content, setContent] = useState('');
-    const [comment, setComment] = useState();
-    const [list,setList]  = useState([]);
+  const router = useRouter();
+  const [subject, setSubject] = useState('');
+  const [content, setContent] = useState('');
+  const [comment, setComment] = useState('');
+  const [list, setList] = useState([]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-          try {
-            const docRef = doc(db, 'articles', router.query.id);
-            const docSnap = await getDoc(docRef);
-            const data = docSnap.data();
-            setSubject(data.subject);
-            setContent(data.content);
-          } catch (error) {
-            // 에러 처리
-          }
-        };
-    
-        fetchData();
-      }, [router.query.id]);
-
-    //댓글 기능
-    const submit = async () => {
-      await addDoc(collection(db, "articles2"), {
-        comment,
-      });
-      alert("저장 되었습니다");
-      setComment("");
-      router.push(`/community/articles/${router.query.id}`);
-      //history.back();
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const docRef = doc(db, 'articles', router.query.id);
+        const docSnap = await getDoc(docRef);
+        const data = docSnap.data();
+        setSubject(data.subject);
+        setContent(data.content);
+      } catch (error) {
+        // 에러 처리
+      }
     };
-    useEffect(() => {
-      onSnapshot(query(collection(db,'articles2')), results => {
-          const newList = [];
-         results.forEach(doc => {
-           const data = doc.data();
-           data.id = doc.id;
-           newList.push(data);
-           console.log(doc.id);
-           console.log(doc.data());
-           console.log('------');
-         });
-         setList(newList)
-      })
-    },[]);
+
+    fetchData();
+  }, [router.query.id]);
+
+  //댓글 기능
+  const submit = async () => {
+    await addDoc(collection(db, "articles2"), {
+      articleId: router.query.id,
+      comment,
+    });
+    alert("저장 되었습니다");
+    setComment("");
+    router.push(`/community/articles/${router.query.id}`);
+    //history.back();
+  };
+
+  useEffect(() => {
+    if (router.query.id) {
+      const q = query(collection(db, 'articles2'), where('articleId', '==', router.query.id));
+
+      const unsubscribe = onSnapshot(q, (results) => {
+        const newList = [];
+        results.forEach((doc) => {
+          const data = doc.data();
+          data.id = doc.id;
+          newList.push(data);
+          console.log(doc.id);
+          console.log(doc.data());
+          console.log('------');
+        });
+        setList(newList);
+      });
+
+      return () => {
+        unsubscribe();
+      };
+    }
+  }, [router.query.id]);
 
     return (
         <div>
