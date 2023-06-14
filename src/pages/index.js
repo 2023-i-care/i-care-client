574a99677ff4d7a60bde25e5ba7619d09acabecf
hyennin <chm050202@gamil.com>
2023-06-14 09:14:48 +0900

    feat: 검색 기능 구현

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,44 +1,37 @@
 import Navbar from '@/components/Navbar';
 import React from 'react';
-import styles from "@/styles/Community.module.css";
+import styles from '@/styles/Community.module.css';
 import db from '../net/db';
-import {collection, getDocs, orderBy, query, onSnapshot} from 'firebase/firestore';
-import { useEffect,useState } from 'react';
-import {DateTime} from 'luxon';
+import { collection, orderBy, query, onSnapshot } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { DateTime } from 'luxon';
 import Link from 'next/link';
 import app from '@/net/firebaseApp';
 
 export default function Home() {
-  const [list,setList]  = useState([]);
+  const [list, setList] = useState([]);
+  const [userInput, setUserInput] = useState('');
+  const [clicked, setClicked] = useState(false);
+  const [posts, setPosts] = useState([]);
+
   useEffect(() => {
-    onSnapshot(query(collection(db,'articles'),orderBy('created_at','desc')), results => {
-        const newList = [];
-       results.forEach(doc => {
-         const data = doc.data();
-         data.id = doc.id;
-         newList.push(data);
-        //  console.log(doc.id);
-        //  console.log(doc.data());
-        //  console.log('------');
-       });
-       setList(newList)
-    })
-    // getDocs(query(collection(db,'articles'),orderBy('created_at','desc')))
-    //   .then(results => {
-    //     const newList = [];
-    //    results.forEach(doc => {
-    //      const data = doc.data();
-    //      data.id = doc.id;
-    //      newList.push(data);
-    //      console.log(doc.id);
-    //      console.log(doc.data());
-    //      console.log('------');
-    //    });
-    //    setList(newList)
-    //   })
-  },[]);
+    onSnapshot(query(collection(db, 'articles'), orderBy('created_at', 'desc')), (results) => {
+      const newList = [];
+      results.forEach((doc) => {
+        const data = doc.data();
+        data.id = doc.id;
+        newList.push(data);
+      });
+      setList(newList);
+      setPosts(newList);
+    });
+  }, []);
 
   const formatDate = (timestamp) => {
+    if (!timestamp) {
+      return ''; // 또는 다른 기본값으로 변경
+    }
+
     const date = DateTime.fromMillis(timestamp);
     const today = DateTime.local().startOf('day');
     if (date >= today) {
@@ -48,49 +41,39 @@ export default function Home() {
     }
   };
 
-  // 검색  
-  const [userInput, setUserInput] = useState('');
-  const [clicked, setClicked] = useState(false);
-
-  // 입력값을 가져와서 소문자로변경
   const getValue = (e) => {
-  setUserInput(e.target.value.toLowerCase())};
+    setUserInput(e.target.value.toLowerCase());
+  };
 
-  // 데이터들을 배열로 posts 에 배열 state로 담아준 상태
-  const [posts, setPosts] = useState([]);
-
-  setPosts((prev) => {
-    list.map((item, idx) => {
-      prev[idx] = item.subject
-    })
-  });
-
-  // 데이터 목록중, name에 사용자 입력값이 있는 데이터만 불러오기
-  // 사용자 입력값을 소문자로 변경해주었기 때문에 데이터도 소문자로
   const searched = posts.filter((item) =>
-    item.subject.toLowerCase().includes(userInput)
+    item.subject && item.subject.toLowerCase().includes(userInput)
   );
 
-  console.log("s: ", searched);
-  console.log("p: ", posts)
+  console.log('s: ', searched);
 
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <div className={styles.container}>
         <div className={styles.top_container}>
           <div className={styles.search_container}>
-            <input className={styles.search_box} onChange={getValue}/>
-            <img className={styles.search_icon} src="/images/search.png" onClick={() => setClicked(true)}/>
+            <input className={styles.search_box} onChange={getValue} />
+            <img
+              className={styles.search_icon}
+              src="/images/search.png"
+              onClick={() => setClicked(true)}
+            />
           </div>
-          <div className={styles.gen_btn}><a href='/community/posting'>글쓰기</a></div>
+          <div className={styles.gen_btn}>
+            <a href="/community/posting">글쓰기</a>
+          </div>
         </div>
         <table className={styles.table}>
           <colgroup>
-            <col style={{width: "10%"}}/>
-            <col style={{width: "70%"}}/>
-            <col style={{width: "10%"}}/>
-            <col style={{width: "10%"}}/>
+            <col style={{ width: '10%' }} />
+            <col style={{ width: '70%' }} />
+            <col style={{ width: '10%' }} />
+            <col style={{ width: '10%' }} />
           </colgroup>
           <thead className={styles.thead}>
             <tr className={styles.tr}>
@@ -101,25 +84,39 @@ export default function Home() {
             </tr>
           </thead>
           <tbody>
-            {clicked ? 
-            searched.map(item => (
-              <tr key={item.id} className={styles.post} onClick={() => location.href = `community/articles/${item.id}`}>
-                <td className={styles.td}><img src='/images/image.png'/></td>
-                <td className={styles.td}>{item.subject}</td>
-                <td className={styles.td}>{item.author}</td>
-                <td className={styles.td}>{formatDate(item.created_at)}</td>
-              </tr>
-            ))
-            :
-            list.map(item => (
-                <tr key={item.id} className={styles.post} onClick={() => location.href = `community/articles/${item.id}`}>
-                  <td className={styles.td}><img src='/images/image.png'/></td>
-                  <td className={styles.td}>{item.subject}</td>
-                  <td className={styles.td}>{item.author}</td>
-                  <td className={styles.td}>{formatDate(item.created_at)}</td>
-                </tr>
-            ))
-            }
+            {clicked
+              ? searched.length > 0 // 검색 결과가 있을 때
+                ? searched.map((item) => {
+                    return (
+                      <tr
+                        key={item.id}
+                        className={styles.post}
+                        onClick={() => (location.href = `community/articles/${item.id}`)}
+                      >
+                        <td className={styles.td}>
+                          <img src="/images/image.png" />
+                        </td>
+                        <td className={styles.td}>{item.subject}</td>
+                        <td className={styles.td}>{item.author}</td>
+                        <td className={styles.td}>{formatDate(item.created_at)}</td>
+                      </tr>
+                    );
+                  })
+                : <tr><td className={styles.no_result} colSpan={4}>검색 결과가 없습니다.</td></tr> // 검색 결과가 없을 때
+              : list.map((item) => (
+                  <tr
+                    key={item.id}
+                    className={styles.post}
+                    onClick={() => (location.href = `community/articles/${item.id}`)}
+                  >
+                    <td className={styles.td}>
+                      <img src="/images/image.png" />
+                    </td>
+                    <td className={styles.td}>{item.subject}</td>
+                    <td className={styles.td}>{item.author}</td>
+                    <td className={styles.td}>{formatDate(item.created_at)}</td>
+                  </tr>
+                ))}
           </tbody>
         </table>
       </div>
