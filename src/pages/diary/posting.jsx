ab110c0dc1a7cf87ed9c9d862f548f365bb354e9
yejin <yejin213527@gmail.com>
2023-06-19 10:04:38 +0900

    다이어리 사진 업로드 기능...

--- src/pages/diary/posting.jsx
+++ src/pages/diary/posting.jsx
@@ -1,36 +1,86 @@
 import styles from "../../styles/DiaryPosting.module.css";
 import Navbar from "@/components/Navbar";
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { collection, addDoc } from "firebase/firestore";
 import db from "../../net/db";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import auth from "@/net/auth";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
 
 const DiaryPosting = () => {
-  const [subject, setSubject] = useState();
-  const [content, setContent] = useState();
-  const [user, setUser] = useState();
+  const [subject, setSubject] = useState("");
+  const [content, setContent] = useState("");
+  const [user, setUser] = useState(null);
+  const [image, setImage] = useState(null);
+  const [imageUrl, setImageUrl] = useState("");
+  const fileInputRef = useRef(null);
   const router = useRouter();
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    setImage(file);
+  };
+
+  const handleImageUpload = async () => {
+    if (!image) return;
+
+    const storage = getStorage();
+    const storageRef = ref(storage, "images/" + image.name);
+    const uploadTask = uploadBytesResumable(storageRef, image);
+
+    try {
+      await uploadTask.on(
+        "state_changed",
+        null,
+        (error) => {
+          console.error("Error uploading image: ", error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setImageUrl(downloadURL);
+            alert("이미지가 업로드되었습니다");
+          });
+        }
+      );
+    } catch (error) {
+      console.error("Error uploading image: ", error);
+    }
+  };
+
   const submit = async () => {
-    await addDoc(collection(db, "articles3"), {
-      subject,
-      content,
-      author: user.email,
-      created_at: new Date().getTime(),
-    });
-    alert("저장 되었습니다");
-    setSubject("");
-    setContent("");
-    router.push("/diary");
-    //history.back();
+    try {
+      const article = {
+        subject,
+        content,
+        author: user?.email || "Unknown User",
+        created_at: new Date().getTime(),
+        imageUrl,
+      };
+      await addDoc(collection(db, "articles3"), article);
+
+      alert("저장 되었습니다");
+      setSubject("");
+      setContent("");
+      setImage(null);
+      setImageUrl("");
+      router.push("/diary");
+    } catch (error) {
+      console.error("Error adding document: ", error);
+    }
   };
 
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
       setUser(user);
     });
+
+    return () => unsubscribe();
   }, []);
 
   return (
@@ -52,11 +102,28 @@ const DiaryPosting = () => {
           onChange={(event) => setContent(event.target.value)}
         />
         <div className={styles.btn_container}>
-          <button className={styles.img_btn}>사진 업로드</button>
+          <div className={styles.file_input_wrapper}>
+            <input
+              type="file"
+              accept="image/*"
+              className={styles.file_input}
+              onChange={handleImageChange}
+              ref={fileInputRef}
+            />
+            <div
+              className={styles.file_input_label}
+              onClick={() => fileInputRef.current.click()}
+            ></div>
+          </div>
           <button className={styles.post_btn} onClick={submit}>
             게시하기
           </button>
         </div>
+        {imageUrl && (
+          <div className={styles.image_preview}>
+            <img src={imageUrl} alt="Uploaded" />
+          </div>
+        )}
       </div>
     </>
   );
