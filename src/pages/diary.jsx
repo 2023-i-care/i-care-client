3f7e2c0166c05893b0bbc539d72ceb1cfb2523d1
hyennin <chm050202@gmail.com>
2023-06-19 14:08:14 +0900

    ✨ feat: 다이어리에 이미지 업로드 구현

--- src/pages/diary.jsx
+++ src/pages/diary.jsx
@@ -4,9 +4,46 @@ import Navbar from "@/components/Navbar";
 import db from '../net/db';
 import {collection, getDocs, orderBy, query, onSnapshot} from 'firebase/firestore';
 import { useEffect,useState } from 'react';
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
+import app from "@/net/firebaseApp";
 
 const Diary = () => {
   const [list,setList]  = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const querySnapshot = await getDocs(
+          query(collection(db, "articles3"), orderBy("created_at", "desc"))
+        );
+  
+        const newList = await Promise.all(querySnapshot.docs.map(async (doc) => {
+          const data = doc.data();
+          data.id = doc.id;
+  
+          // 이미지 URL 가져오기
+          if (data.image) {
+            const storage = getStorage(app); // Storage 인스턴스 가져오기
+            const storageRef = ref(storage, data.image);
+            try {
+              const url = await getDownloadURL(storageRef);
+              data.imageURL = url;
+            } catch (error) {
+              console.error("Error getting image URL:", error);
+            }
+          }
+  
+          return data;
+        }));
+  
+        setList(newList);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+  
+    fetchData();
+  }, []);
+  
   useEffect(() => {
     onSnapshot(query(collection(db,'articles3'),orderBy('created_at','desc')), results => {
         const newList = [];
@@ -14,9 +51,6 @@ const Diary = () => {
          const data = doc.data();
          data.id = doc.id;
          newList.push(data);
-         console.log(doc.id);
-         console.log(doc.data());
-         console.log('------');
        });
        setList(newList)
     })
@@ -32,7 +66,9 @@ const Diary = () => {
               <div className={styles.title}>{item.subject}</div>
               <div className={styles.content}>{item.content}</div>
             </div>
-            <div className={styles.image}></div>
+            {/* {item.imageURL && (
+              <img src={item.imageURL} alt="image" className={styles.image} />
+            )} */}
           </div>
           ))}
       </div>
